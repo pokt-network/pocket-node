9c55826f253dc77fd07ac2025ea39441c029a6da
Luis de Leon <luyzdeleon@gmail.com>
2017-11-02 18:14:19 -0400

    Added relayer functionality

--- controllers/relay.js
+++ controllers/relay.js
@@ -1,6 +1,14 @@
 'use strict';
 
 module.exports.add = async function(ctx, next) {
-  ctx.body = {};
+  var relay_params = ctx.request.body,
+      RelayerClass = require('pkt-relayer-' + relay_params['token'].toLowerCase());
+  if (RelayerClass) {
+    var Relayer = new RelayerClass(relay_params['transaction']),
+        transaction_hash = Relayer.relay_signed_transaction();
+    ctx.body = {
+      transaction_hash: transaction_hash
+    }
+  }
   await next();
 };
