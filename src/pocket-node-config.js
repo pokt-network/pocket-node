abfb318a2a75c6ccef80b609bd449a67e3d1e6fd
Luis de Leon <luyzdeleon@gmail.com>
2018-04-02 14:02:17 -0400

    Added configuration mechanism

--- src/pocket-node-config.js
+++ src/pocket-node-config.js
@@ -1,9 +1,31 @@
-// TODO: Implement this
-module.exports.getSupportedNetworks = function(){
-  return [];
+var updater = require('jsonfile-updater'),
+    configFilePath = '../configuration/config.json',
+    configFile = require(configFilePath);
+
+// Private module functions
+function updateConfigValue(key, value){
+  updater(configFilePath).update(key, value, function(err) {
+    if (err) {
+      const errorMessage = 'Failed to update configuration property: ' + key;
+      throw errorMessage;
+    }
+  });
 }
 
-// TODO: Implement this
+function reloadConfigFile(){
+  configFile = require(configFilePath);
+}
+
+function getProperty(key){
+  reloadConfigFile();
+  return configFile[key];
+}
+
+// Public module functions
+module.exports.getSupportedNetworks = function() {
+  return getProperty('networks') || [];
+}
+
 module.exports.isNetworkSupported = function(network) {
-  return true;
+  return this.getSupportedNetworks().includes(network.toUpperCase());
 }
