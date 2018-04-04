e9a67efb68a83e588439639a95e0959bb4234a17
Luis de Leon <luyzdeleon@gmail.com>
2018-04-04 13:13:26 -0400

    Made contract instance getting async

--- src/contracts/epoch-registry-api.js
+++ src/contracts/epoch-registry-api.js
@@ -1,20 +1,32 @@
 var PocketNodeConfig = require('../pocket-node-config'),
     Web3 = require('web3'),
-    ethProvider = new Web3.providers.HttpProvider(PocketNodeConfig.getEthProviderURL()),
     TruffleContract = require("truffle-contract"),
-    contractABI = require('./epoch-registry-api-abi.json');
-
-// Create contract abstraction
-var EpochRegistryAPI = TruffleContract({
-  abi: contractABI,
-  address: PocketNodeConfig.getEpochRegistryAPIAddress()
-});
-
-// Configure contract instance
-EpochRegistryAPI.setProvider(ethProvider);
-EpochRegistryAPI.defaults({
-  from: PocketNodeConfig.getEthAccount()
-});
+    contractABI = require('./epoch-registry-api-abi.json').abi;
 
 // Export contract instance
-module.exports = EpochRegistryAPI;
+module.exports = async function(){
+  var ethProviderUrl = await PocketNodeConfig.getEthProviderURL(),
+      ethProvider = new Web3.providers.HttpProvider(ethProviderUrl),
+      networkId = await PocketNodeConfig.getEthNetworkId(),
+      contractAddress = await PocketNodeConfig.getEpochRegistryAPIAddress(),
+      contractInstance = null;
+
+  // Create contract abstraction
+  var EpochRegistryAPI = TruffleContract({
+    abi: contractABI
+  });
+
+  // Configure contract instance
+  EpochRegistryAPI.setProvider(ethProvider);
+  EpochRegistryAPI.setNetwork(parseInt(networkId));
+  EpochRegistryAPI.defaults({
+    from: await PocketNodeConfig.getEthAccount()
+  });
+
+  try {
+    contractInstance = await EpochRegistryAPI.at(contractAddress);
+  } catch (e) {
+    console.error(e);
+  }
+  return contractInstance;
+}
