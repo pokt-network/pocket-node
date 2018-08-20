fe6133ed50d0545a082107ab0ee9b40b11bb27ac
Luis de Leon <luyzdeleon@gmail.com>
2018-08-20 11:59:03 -0400

    Added subnetwork to tests

--- test/index.js
+++ test/index.js
@@ -15,6 +15,7 @@ describe('Pocket Node Server', function() {
     pocketNodeServer = new PocketNodeServer(POCKET_NODE_SERVER_PORT, null);
     pocketNodeServer.start(async function() {
       await PluginManager.registerPlugin(path.join(appRootPath) + '/test/test_plugin');
+      await PluginManager.configurePlugin('TEST', {'5777': {test_config_1: "test value"}});
       done();
     });
   });
@@ -45,12 +46,13 @@ describe('Pocket Node Server', function() {
     it('should submit a valid transaction', function() {
       return request(pocketNodeServer.httpServer)
         .post('/transactions')
-        .send({network: "TEST", serialized_tx: "0x000", tx_metadata: {}})
+        .send({network: "TEST", subnetwork: '5777', serialized_tx: "0x000", tx_metadata: {}})
         .set('Accept', 'application/json')
         .expect(200)
         .then(response => {
           const expectedResponse = {
                                       network: 'TEST',
+                                      subnetwork: '5777',
                                       serialized_tx: '0x000',
                                       tx_metadata: {},
                                       hash: '0x0',
@@ -59,6 +61,7 @@ describe('Pocket Node Server', function() {
                                       error_msg: null
                                     };
           assert.equal(response.body.network, expectedResponse.network);
+          assert.equal(response.body.subnetwork, expectedResponse.subnetwork);
           assert.equal(response.body.serialized_tx, expectedResponse.serialized_tx);
           //assert.equal(response.body.tx_metadata, expectedResponse.tx_metadata);
           assert.equal(response.body.hash, expectedResponse.hash);
@@ -84,18 +87,20 @@ describe('Pocket Node Server', function() {
     it('should execute a valid query', function() {
       return request(pocketNodeServer.httpServer)
         .post('/queries')
-        .send({network: "TEST", serialized_tx: "0x000", tx_metadata: {}})
+        .send({network: "TEST", subnetwork: '5777', serialized_tx: "0x000", tx_metadata: {}})
         .set('Accept', 'application/json')
         .expect(200)
         .then(response => {
           const expectedResponse = {
                                       network: 'TEST',
+                                      subnetwork: '5777',
                                       result: true,
                                       decoded: false,
                                       error: false,
                                       error_msg: null
                                    };
           assert.equal(response.body.network, expectedResponse.network);
+          assert.equal(response.body.subnetwork, expectedResponse.subnetwork);
           assert.equal(response.body.result, expectedResponse.result);
           assert.equal(response.body.decoded, expectedResponse.decoded);
           assert.equal(response.body.error, expectedResponse.error);
