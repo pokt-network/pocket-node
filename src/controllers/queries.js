2ed140c200915ccc317d89f2362d60ab4d607d1b
Luis de Leon <luyzdeleon@gmail.com>
2018-08-20 11:22:02 -0400

    Added query subnetwork support

--- src/controllers/queries.js
+++ src/controllers/queries.js
@@ -1,10 +1,12 @@
 var PluginManager = require('../plugin-manager');
 
 const NETWORK_NOT_SUPPORTED_ERROR = 'Network not supported';
+const SUB_NETWORK_NOT_SUPPORTED_ERROR = 'Subnetwork not supported';
 
 function defaultCreateResponse(queryRequest) {
   return {
     network: queryRequest.network,
+    subnetwork: queryRequest.subnetwork,
     query: queryRequest.query,
     decoder: queryRequest.decoder,
     result: null,
@@ -24,29 +26,39 @@ module.exports.submit = async function(ctx, next) {
   // Get request
   var queryRequest = ctx.request.body,
       queryNetwork = queryRequest.network.toUpperCase(),
+      querySubnetwork = queryRequest.subnetwork.toString(),
       plugin = await PluginManager.getPlugin(queryNetwork),
-      pluginData = await PluginManager.getPluginData(queryNetwork);
+      pluginData = await PluginManager.getPluginData(queryNetwork),
+      pluginSubnetworkConfiguration = pluginData.configuration[querySubnetwork];
 
   // Set default response
   ctx.body = defaultCreateResponse(queryRequest);
 
-  // Check if plugin is available
+  // Plugin and configuration checks
   if (!plugin) {
     setErrorResponse(ctx, 500, NETWORK_NOT_SUPPORTED_ERROR);
-  } else {
-    // Submit transaction
-    const queryResponse = await plugin.executeQuery(queryRequest.query, queryRequest.decoder, pluginData['configuration'] || {});
+    await next();
+    return
+  }
 
-    // Parse result
-    ctx.body.result = queryResponse.result;
-    ctx.body.decoded = queryResponse.decoded;
-    ctx.body.error = queryResponse.error;
-    ctx.body.error_msg = queryResponse.errorMsg;
+  if (!pluginSubnetworkConfiguration) {
+    setErrorResponse(ctx, 500, SUB_NETWORK_NOT_SUPPORTED_ERROR);
+    await next();
+    return
+  }
 
-    // Error out if needed
-    if (queryResponse.error) {
-      setErrorResponse(ctx, 400, ctx.body.error_msg);
-    }
+  // Submit transaction
+  const queryResponse = await plugin.executeQuery(queryRequest.query, queryRequest.decoder, pluginSubnetworkConfiguration);
+
+  // Parse result
+  ctx.body.result = queryResponse.result;
+  ctx.body.decoded = queryResponse.decoded;
+  ctx.body.error = queryResponse.error;
+  ctx.body.error_msg = queryResponse.errorMsg;
+
+  // Error out if needed
+  if (queryResponse.error) {
+    setErrorResponse(ctx, 400, ctx.body.error_msg);
   }
 
   await next();
