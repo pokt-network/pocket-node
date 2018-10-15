5504df3c1b21303451e2ea7e4639156e62510e46
aqt01 <lowell.abbott@gmail.com>
2018-10-15 00:54:49 -0400

    Add configuration for connecting to GETH testnet and mainnet

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,15 @@ if [ "${POCKET_NODE_PLUGIN_ETH:-enabled}" = "enabled" ]; then
 
   cat <<EOF > ${POCKET_NODE_CONFIGURATION_DIR}/eth.json
 {
-  "${POCKET_NODE_PLUGIN_ETH_NETWORK_ID:-4}": {
-    "eth_node": "${POCKET_NODE_PLUGIN_ETH_NODE_URL:-http://geth:8545}",
-    "eth_network_id": "${POCKET_NODE_PLUGIN_ETH_NETWORK_ID:-4}"
+  "${POCKET_NODE_PLUGIN_ETH_MAIN_NETWORK_ID:-1}": {
+    "eth_node": "${POCKET_NODE_PLUGIN_ETH_MAIN_NODE:-http://geth:8545}",
+    "eth_network_id": "${POCKET_NODE_PLUGIN_ETH_MAIN_NETWORK_ID:-1}"
+  },
+  "${POCKET_NODE_PLUGIN_ETH_TEST_NETWORK_ID:-4}": {
+    "eth_node": "${POCKET_NODE_PLUGIN_ETH_TESTNET_NODE:-http://geth:8545}",
+    "eth_network_id": "${POCKET_NODE_PLUGIN_ETH_TEST_NETWORK_ID:-4}"
   }
+
 }
 
 EOF
