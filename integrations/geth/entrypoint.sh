5f8fc6bee5e5150eab43a29fd249a8239385e665
Luis de Leon <luyzdeleon@gmail.com>
2018-10-16 20:12:48 -0400

    Fixed environment variable name

--- integrations/geth/entrypoint.sh
+++ integrations/geth/entrypoint.sh
@@ -11,7 +11,7 @@ if [ "${POCKET_NODE_PLUGIN_ETH:-enabled}" = "enabled" ]; then
   cat <<EOF > ${POCKET_NODE_CONFIGURATION_DIR}/eth.json
 {
   "${POCKET_NODE_PLUGIN_ETH_MAIN_NETWORK_ID:-1}": {
-    "eth_node": "${POCKET_NODE_PLUGIN_ETH_MAIN_NODE:-http://geth-mainnet:8545}",
+    "eth_node": "${POCKET_NODE_PLUGIN_ETH_MAINNET_NODE:-http://geth-mainnet:8545}",
     "eth_network_id": "${POCKET_NODE_PLUGIN_ETH_MAIN_NETWORK_ID:-1}"
   },
   "${POCKET_NODE_PLUGIN_ETH_TEST_NETWORK_ID:-4}": {
