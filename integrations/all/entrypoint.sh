e49f6724664a8a6942e9f491bd75e8c4f5fcafac
aqt01 <lowell.abbott@gmail.com>
2019-01-17 13:33:17 -0400

    Add mapping 3000 with localhost when running pocket-node in all integrations. Also using correct image in aion service on all integrations

--- integrations/all/entrypoint.sh
+++ integrations/all/entrypoint.sh
@@ -30,13 +30,13 @@ if [ "${POCKET_NODE_PLUGIN_AION:-enabled}" = "enabled" ]; then
 
   cat <<EOF > ${POCKET_NODE_CONFIGURATION_DIR}/aion.json
 {
-  "${POCKET_NODE_PLUGIN_AION_MAIN_NETWORK_ID:-1}": {
-    "aion_node": "${POCKET_NODE_PLUGIN_AION_MAINNET_NODE:-http://aion-mainnet:8545}",
-    "network_id": "${POCKET_NODE_PLUGIN_AION_MAIN_NETWORK_ID:-1}"
+  "${POCKET_NODE_PLUGIN_AION_MAIN_NETWORK_ID:-256}": {
+    "aion_node": "${POCKET_NODE_PLUGIN_AION_MAIN_NODE:-http://aion-mainnet:8545}",
+    "network_id": "${POCKET_NODE_PLUGIN_AION_MAIN_NETWORK_ID:-256}"
   },
-  "${POCKET_NODE_PLUGIN_AION_TEST_NETWORK_ID:-2}": {
+  "${POCKET_NODE_PLUGIN_AION_TEST_NETWORK_ID:-32}": {
     "aion_node": "${POCKET_NODE_PLUGIN_AION_TESTNET_NODE:-http://aion-mastery:8545}",
-    "network_id": "${POCKET_NODE_PLUGIN_AION_TEST_NETWORK_ID:-2}"
+    "network_id": "${POCKET_NODE_PLUGIN_AION_TEST_NETWORK_ID:-32}"
   }
 
 }
